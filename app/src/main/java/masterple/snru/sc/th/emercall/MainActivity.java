19032d7efc68fbdb472f1691dcd8b4766d52c919
sutheekanPLE <sutneekan.th59@snru.an.th>
2018-02-20 12:01:33 +0700

    Add Fragment Success

--- app/src/main/java/masterple/snru/sc/th/emercall/MainActivity.java
+++ app/src/main/java/masterple/snru/sc/th/emercall/MainActivity.java
@@ -3,11 +3,26 @@ package masterple.snru.sc.th.emercall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterple.snru.sc.th.emercall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+//    Add Fragment
+            if (savedInstanceState == null) {
+
+                getSupportFragmentManager().beginTransaction()
+                        .add(R.id.contentMainFeagment, new MainFragment()).commit();
+
+
+        }
+
+
+}  //Main Method
+
+}// Main Class
