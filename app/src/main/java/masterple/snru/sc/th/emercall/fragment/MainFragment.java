8b9d645cae9756955c3eb1d784320e3fef475d20
sutheekanPLE <sutneekan.th59@snru.an.th>
2018-02-20 11:31:43 +0700

    Create Fragment Success

---
+++ app/src/main/java/masterple/snru/sc/th/emercall/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package masterple.snru.sc.th.emercall.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import masterple.snru.sc.th.emercall.R;
+
+/**
+ * Created by Admin on 20/2/2561.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_police, container, false);
+        return view;
+    }
+}  //Main Class
